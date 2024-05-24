f6107e9c4487e5dee2567a7fdaa983e8f5c1a8e6
MaxI <makcimka1477@yandex.ru>
2024-05-24 11:21:34 +0300

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+import path from 'path';
+
+const __dirname = new URL('.', import.meta.url).pathname;
+
 const nextConfig = {
-images: {
+  images: {
     remotePatterns: [
       {
         protocol: 'https',
@@ -9,7 +14,15 @@ images: {
         pathname: '/*/**',
       },
     ],
-  }
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'),
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
